dcb36deebdfef59800f64ba1e7e4e133324ca1bb
Wallan Pereira <32087983+wallanpsantos@users.noreply.github.com>
2021-07-21 13:18:16 -0300

    Adicionado Projeto Base

---
+++ src/main/java/com/br/projetoSpringBootTDD/ProjetoSpringBootTddApplication.java
@@ -0,0 +1,13 @@
+package com.br.projetoSpringBootTDD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringBootTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringBootTddApplication.class, args);
+	}
+
+}
