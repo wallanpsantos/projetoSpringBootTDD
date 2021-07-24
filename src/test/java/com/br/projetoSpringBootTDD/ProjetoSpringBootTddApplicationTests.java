98e230d177dd0033b21c6c9b51048f4602d405fa
Wallan Pereira <32087983+wallanpsantos@users.noreply.github.com>
2021-07-24 13:29:42 -0300

    Ajuste Pom.xml

--- src/test/java/com/br/projetoSpringBootTDD/ProjetoSpringBootTddApplicationTests.java
+++ src/test/java/com/br/projetoSpringBootTDD/ProjetoSpringBootTddApplicationTests.java
@@ -1,5 +1,6 @@
 package com.br.projetoSpringBootTDD;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,14 @@ class ProjetoSpringBootTddApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// cenario
+		Integer x = 10, y = 20;
+
+		// execução
+		Integer res = x + y;
+
+		// validação
+		Assertions.assertThat(res).isGreaterThan(5);
 	}
 
 }
