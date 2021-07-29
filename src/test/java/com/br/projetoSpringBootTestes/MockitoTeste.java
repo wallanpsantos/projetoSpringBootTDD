81a6a07e1ea69f997ee30730ad8bee9d518d9550
Wallan Pereira <32087983+wallanpsantos@users.noreply.github.com>
2021-07-29 02:08:39 -0300

    Iniciando no Mockito

---
+++ src/test/java/com/br/projetoSpringBootTestes/MockitoTeste.java
@@ -0,0 +1,24 @@
+package com.br.projetoSpringBootTestes;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+
+public class MockitoTeste {
+
+    @Test
+    public void primeiroTesteMockito() {
+        var lista = Mockito.mock(ArrayList.class);
+
+        lista.add("Luffy");
+        lista.add("Nami");
+
+        Mockito.when(lista.size()).thenReturn(20);
+
+        int tamanho = lista.size();
+
+        Assertions.assertThat(tamanho).isEqualTo(20);
+    }
+}
